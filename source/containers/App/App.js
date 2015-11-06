4345728ffbea3048d28e47c30be9d7f692378073
alexbeletsky <alexander.beletsky@gmail.com>
2015-11-06 16:52:12 +0100

    moved to components

--- source/containers/App/App.js
+++ source/containers/App/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import { HelloWorld } from '../../components';
 
 export default class App extends Component {
   render() {
     return (
-      <h1>Hello, world</h1>
+      <HelloWorld></HelloWorld>
     );
   }
 }
