a57f4fdea5dd32ca4697f15f79ac70552f56519f
alexbeletsky <alexander.beletsky@gmail.com>
2015-11-08 16:17:49 +0100

    introduced router

--- source/router/index.js
+++ source/router/index.js
@@ -1,5 +1,21 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import App from '../containers/App/App';
 
-ReactDom.render(<App />, document.getElementById('app'));
+import Router, {Route} from 'react-router';
+import createBrowserHistory from 'history/lib/createBrowserHistory';
+
+import {App} from '../containers';
+
+let renderRoutes = (element) => {
+  const Routes = (
+    <Router history={createBrowserHistory()}>
+      <Route path="/" component={App}></Route>
+    </Router>
+  );
+
+  ReactDom.render(Routes, element);
+};
+
+let root = document.getElementById('main');
+
+renderRoutes(root);
