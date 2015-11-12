dc2e12914d5e082b65732fa4f0893605f355786f
alexbeletsky <alexander.beletsky@gmail.com>
2015-11-12 18:04:44 +0100

    styling

--- source/components/SignUpWizard/BloggingPreferences.js
+++ source/components/SignUpWizard/BloggingPreferences.js
@@ -1,6 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 
-import { Checkbox } from 'material-ui';
+import { RaisedButton, Checkbox } from 'material-ui';
 
 class BloggingPreferences extends Component {
   static propTypes = {
@@ -8,32 +8,40 @@ class BloggingPreferences extends Component {
   }
 
   render() {
+    const buttonsStyle = {
+      marginTop: 30,
+      textAlign: 'right'
+    };
+
     return (
       <div className="blogging-prerences">
         <div style={{ textAlign: 'center' }}>
           <h2>Blogging Preferences</h2>
         </div>
-        <div>
+        <div style={{ height: 400, paddingTop: 50 }}>
           <ul style={{ listStyleType: 'none' }}>
-            <li style={{ display: 'inline' }}>
+            <li style={{ display: 'inline-block', paddingTop: 5, paddingBottom: 5, width: '33%' }}>
               <Checkbox
                 name="food"
                 value="checkboxValue1"
                 label="Food blogger"/>
             </li>
-            <li style={{ display: 'inline' }}>
+            <li style={{ display: 'inline-block', paddingTop: 5, paddingBottom: 5, width: '33%' }}>
               <Checkbox
                 name="fashion"
                 value="checkboxValue1"
                 label="Fashion blogger"/>
             </li>
-            <li style={{ display: 'inline' }}>
+            <li style={{ display: 'inline-block', paddingTop: 5, paddingBottom: 5, width: '33%' }}>
               <Checkbox
                 name="food"
                 value="checkboxValue1"
                 label="Sports car blogger"/>
             </li>
           </ul>
+        </div>
+        <div style={buttonsStyle}>
+          <RaisedButton label="Next" type="submit" secondary/>
         </div>
       </div>
     );
